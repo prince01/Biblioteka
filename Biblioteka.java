344b419f3e97a0f38d6a9817bad7c3f4652bf588
prince01 <kamiltront@tlen.pl>
2013-12-05 17:20:44 +0100

    little changes

--- Biblioteka.java
+++ Biblioteka.java
@@ -1,15 +1,8 @@
 package client;
 
-import java.io.BufferedReader;
-
-import java.io.FileReader;
-
-import java.io.IOException;
-import java.io.StreamTokenizer;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.StringTokenizer;
 
 public class Biblioteka {
     String adresBiblioteki;
